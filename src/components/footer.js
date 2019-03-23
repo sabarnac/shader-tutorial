b3df7c3a17a76e4d35783bbd8a648aecc5d94776
Sabarna Chakravarty <sabarnac@gmail.com>
2019-03-23 18:19:19 +0530

    Add Icon Source In Footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,16 @@ import twitter from "../images/twitter.png"
 const Footer = () => (
   <div className="row">
     <footer className="column">
-      <div className="author">By Sabarna Chakravarty</div>
+      <div className="author">
+        By Sabarna Chakravarty | Logo from{" "}
+        <a
+          href="https://commons.wikimedia.org/wiki/File:Phong-shading-sample.jpg"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          Wikimedia Commons
+        </a>
+      </div>
       <div className="social">
         <a
           href="https://twitter.com/disappointin_af"
