333b75276491ae9ecaab3b16524fa9059aac1d17
Sabarna Chakravarty <sabarnac@gmail.com>
2019-03-23 18:19:16 +0530

    Show Icon In Header

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,14 @@
 import PropTypes from "prop-types"
 import React from "react"
 import Navigation from "./navigation"
+import icon from "../images/icon.png"
 
 const Header = ({ siteTitle }) => (
   <header className="column column-25">
+    <img
+      src={icon}
+      style={{ display: "block", maxWidth: "65%", margin: "0 auto" }}
+    />
     <h1>{siteTitle}</h1>
     <Navigation />
   </header>
