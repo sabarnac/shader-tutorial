f02406c6a55b2873a896f9c4a31cef0c40e2452f
Sabarna Chakravarty <sabarnac@gmail.com>
2019-04-01 15:52:42 +0530

    Update home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,15 +16,20 @@ const IndexPage = ({ location: { pathname } }) => (
       <h2>Welcome!</h2>
       <p>
         The purpose of this website is to provide a simple understanding about
-        the concept of GPU shaders, their types, purpose, and functionality, and
-        a look into how they work and can be used to perform a variety of
-        operations that can create effects or perform certain actions that
-        leverage the power of a GPU.
+        the concept of GPU shaders, their types, purpose, and how they work and
+        can be used to perform a variety of operations that can create effects
+        or perform certain actions that leverage the power of a GPU.
       </p>
       <p>
         The tutorials are supposed to provide an understanding on the concepts
         and functions of GPU shaders, while keeping things relatively simple and
         as language agnostic as possible.
+      </p>
+      <p>
+        Do note that this tutorial was written from the perspective of video
+        games and effects and functionality required or useful for them. The
+        concepts taught can still be used in other areas, and content may be
+        updated to make the target application more general.
       </p>
       <p>
         You can browse through the tutorial by clicking on the links in the
