e2d3b1f7b4ae181da957d0a36f60e6908f217993
Sabarna Chakravarty <sabarnac@gmail.com>
2020-01-05 17:24:09 +0530

    Update Content

--- src/pages/intermediates/specular-mapping.js
+++ src/pages/intermediates/specular-mapping.js
@@ -1,3 +1,4 @@
+import { Link } from "gatsby";
 import React from "react";
 
 import Content from "../../components/content";
@@ -21,7 +22,19 @@ const SpecularMappingPage = ({ location: { pathname } }) => (
     />
     <Content>
       <h2>Shader Intermediates - Specular Mapping</h2>
-      <h3>Example - Stone wall</h3>
+      <p>
+        Similar to how we've stored information the normal information of a
+        surface in a map (as taught in the{" "}
+        <Link to="/intermediates/normal-mapping">normal mapping chapter</Link>),
+        we can also store specular reflectivity information of a surface in a
+        map as well. Such a map is called a specular map.
+      </p>
+      <h3>Specular Maps</h3>
+      <p>
+        Like the texture maps used in the{" "}
+        <Link to="/intermediates/normal-mapping">normal mapping chapter</Link>,
+        the texture maps that will be used to color and light the wall are:
+      </p>
       <p className="util text-center">
         <img
           alt="Stone Wall - Color Map"
@@ -29,7 +42,7 @@ const SpecularMappingPage = ({ location: { pathname } }) => (
           src={texture}
         />
         <br />
-        Color/Diffuse map
+        Diffuse map
       </p>
       <p className="util text-center">
         <img
@@ -49,6 +62,32 @@ const SpecularMappingPage = ({ location: { pathname } }) => (
         <br />
         Specular map
       </p>
+      <p>
+        The specular map is a grayscale image that contains the following
+        information per pixel:
+      </p>
+      <ul>
+        <li>
+          The value of the specular reflectivity of the surface is stored as a
+          grayscale color value.
+        </li>
+        <li>
+          The range of the color goes from 0 to 255 (0.0 - 1.0 in float),
+          mapping into the range 0% - 100% specular reflectivity of the surface
+          at that point.
+        </li>
+      </ul>
+      <p>
+        This means that the brighter a point is in a specular map, the smoother
+        the surface is at that point, and the higher the specular lighting will
+        be at that point (more of the light is directly reflected towards the
+        camera).
+      </p>
+      <p>
+        Let's look at the stone wall example with the specular map added in.
+        We'll be performing the lighting calculation in view-space.
+      </p>
+      <h3>Example - Stone wall</h3>
       <SpecularMappingFirstExample />
       <GlslCodeHighlight
         code={firstVertexShaderSource.trim()}
@@ -58,8 +97,39 @@ const SpecularMappingPage = ({ location: { pathname } }) => (
         code={firstFragmentShaderSource.trim()}
         type={"Fragment"}
       />
+      <p>
+        At line 37, we grab the specular color value from the specular map, and
+        use that as the specular reflectivity value for the specular lighting
+        component.
+      </p>
+      <p>
+        Since the colors stored in the specular map are in grayscale from the
+        range of 0.0 to 1.0, the RGB value can be used as is and multiplied
+        against the specular lighting vector.
+      </p>
       <h3>Summary</h3>
-      <ul></ul>
+      <ul>
+        <li>
+          Similar to normal mapping, specular mapping can be used to map
+          specular reflectivity to fragments inside a polygon to make a surface
+          look more reflective and smooth.
+        </li>
+        <li>
+          Specular maps add the apperence of smoothness on the surface of
+          objects, allowing for the surface to reflect more light directly,
+          increasing the specular lighting of the surface.
+        </li>
+        <li>
+          The reflectivity of each point are stored in the image as a grayscale
+          color value.
+        </li>
+        <li>
+          The color values representing the specular reflectivity for each
+          fragment are retrieved from the texture and used as is, since the
+          colors are in grayscale and can be used directly to calculate how much
+          specular light is actually reflected.
+        </li>
+      </ul>
     </Content>
     <PageChange previous="/intermediates/normal-mapping/" />
   </Layout>
