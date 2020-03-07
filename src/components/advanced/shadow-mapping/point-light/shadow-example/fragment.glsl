795d08d6c23d3bb78c0b2672aaef8d04fb97eb22
Sabarna Chakravarty <sabarnac@gmail.com>
2020-03-08 00:09:14 +0530

    Add Multi-Sampling To Point Light Example

--- src/components/advanced/shadow-mapping/point-light/shadow-example/fragment.glsl
+++ src/components/advanced/shadow-mapping/point-light/shadow-example/fragment.glsl
@@ -8,12 +8,22 @@ uniform highp float ambientFactor;
 uniform highp float farPlane;
 uniform samplerCube shadowTextureSampler;
 
-const highp float acneBias = 0.01;
+const highp float acneBias = 0.02;
 
 highp float getAverageVisibility() {
   highp vec3 vertexDirection_worldSpace = vertexPosition_worldSpace - lightPosition_worldSpace.xyz;
-  highp float closestDepth = textureCube(shadowTextureSampler, vertexDirection_worldSpace.xyz).z;
-  return (length(vertexDirection_worldSpace) / farPlane) - acneBias > closestDepth ? 0.0 : 1.0;
+  highp float currentDepth = length(vertexDirection_worldSpace) / farPlane;
+
+  highp float visibility = 0.0;
+  for (int x = -1; x <= 1; x++) {
+    for (int y = -1; y <= 1; y++) {
+      for (int z = -1; z <= 1; z++) {
+        highp float closestDepth = textureCube(shadowTextureSampler, vertexDirection_worldSpace.xyz + (vec3(x, y, z) * 0.05)).z;
+        visibility += currentDepth - acneBias > closestDepth ? 0.0 : 1.0;
+      }
+    }
+  }
+  return visibility / 27.0;
 }
 
 void main() {
