d8fac9756ebc8308ae27b26cf02d2951cb2f923d
Sabarna Chakravarty <sabarnac@gmail.com>
2020-03-11 17:20:38 +0530

    Update Point Light Anti-Aliasing

--- src/components/advanced/shadow-mapping/point-light/shadow-example/fragment.glsl
+++ src/components/advanced/shadow-mapping/point-light/shadow-example/fragment.glsl
@@ -15,15 +15,15 @@ highp float getAverageVisibility() {
   highp float currentDepth = length(vertexDirection_worldSpace) / farPlane;
 
   highp float visibility = 0.0;
-  for (int x = -1; x <= 1; x++) {
-    for (int y = -1; y <= 1; y++) {
-      for (int z = -1; z <= 1; z++) {
-        highp float closestDepth = textureCube(shadowTextureSampler, vertexDirection_worldSpace.xyz + (vec3(x, y, z) * 0.05)).z;
-        visibility += currentDepth - acneBias > closestDepth ? 0.0 : 1.0;
-      }
-    }
+  for (int i = -1; i <= 1; i++) {
+    highp float closestDepthXBias = textureCube(shadowTextureSampler, vertexDirection_worldSpace.xyz + (vec3(i, 0, 0) * 0.05)).z;
+    highp float closestDepthYBias = textureCube(shadowTextureSampler, vertexDirection_worldSpace.xyz + (vec3(0, i, 0) * 0.05)).z;
+    highp float closestDepthZBias = textureCube(shadowTextureSampler, vertexDirection_worldSpace.xyz + (vec3(0, 0, i) * 0.05)).z;
+    visibility += currentDepth - acneBias > closestDepthXBias ? 0.0 : 1.0;
+    visibility += currentDepth - acneBias > closestDepthYBias ? 0.0 : 1.0;
+    visibility += currentDepth - acneBias > closestDepthZBias ? 0.0 : 1.0;
   }
-  return visibility / 27.0;
+  return visibility / 9.0;
 }
 
 void main() {
