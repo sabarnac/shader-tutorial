fcc0ce6408873d1de79b2703df78dca5ce9563e2
Sabarna Chakravarty <sabarnac@gmail.com>
2021-09-01 00:44:34 +0530

    Update Header

--- src/components/header.js
+++ src/components/header.js
@@ -1,40 +1,48 @@
 import PropTypes from "prop-types"
-import React from "react"
+import React, { useEffect, useState } from "react"
 
 import animatedIcon from "../images/icon-animated.png"
 import icon from "../images/icon.png"
 import Navigation from "./navigation"
 
-const Header = ({ siteTitle, isHomePage }) => (
-  <header className="column column-25">
-    <div style={{ display: "flex", justifyContent: "center" }}>
-      <picture>
-        <source srcSet={animatedIcon} type="image/apng" />
-        <img
-          src={icon}
-          alt="GPU Shader Tutorial Logo"
-          height={256}
-          width={256}
-          style={{ width: "256px", height: "100%" }}
-        />
-      </picture>
-    </div>
-    {isHomePage ? (
-      <h1 id="site-title" className="title-largest">
-        {siteTitle}
-      </h1>
-    ) : (
-      <div
-        id="site-title"
-        class="title-largest"
-        style={{ textAlign: "center" }}
-      >
-        {siteTitle}
+const Header = ({ siteTitle, isHomePage }) => {
+  const [showAnimatedIcon, setShowAnimatedIcon] = useState(false)
+
+  useEffect(() => setShowAnimatedIcon(true), [])
+
+  return (
+    <header className="column column-25">
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <picture>
+          {showAnimatedIcon ? (
+            <source srcSet={animatedIcon} type="image/apng" />
+          ) : null}
+          <img
+            src={icon}
+            alt="GPU Shader Tutorial Logo"
+            height={256}
+            width={256}
+            style={{ width: "256px", height: "100%" }}
+          />
+        </picture>
       </div>
-    )}
-    <Navigation />
-  </header>
-)
+      {isHomePage ? (
+        <h1 id="site-title" className="title-largest">
+          {siteTitle}
+        </h1>
+      ) : (
+        <div
+          id="site-title"
+          class="title-largest"
+          style={{ textAlign: "center" }}
+        >
+          {siteTitle}
+        </div>
+      )}
+      <Navigation />
+    </header>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
